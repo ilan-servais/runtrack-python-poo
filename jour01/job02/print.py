807005613b290b0c5da66fd53d57850cd16bdaf1
ilan-servais <145970490+ilan-servais@users.noreply.github.com>
2024-01-03 19:21:02 +0100

    day1
    day1

---
+++ jour01/job02/print.py
@@ -0,0 +1,11 @@
+class Operation:
+
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+
+operation = Operation(12, 3)
+
+print(operation.nombre1)
+print(operation.nombre2)
